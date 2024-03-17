a0512da4e3b4df345dddd2ffaae73ce9114ee085
Aarush Paul <76874133+aarush-paul@users.noreply.github.com>
2024-03-18 01:28:44 +0530

    upload

---
+++ README.md
@@ -0,0 +1,63 @@
+
+![clackr](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
+
+
+# Clackr
+
+A command line based chat app made entirely in Javascript using NodeJS.
+
+
+## Installation
+
+Install Clackr with npm
+
+```bash
+  npm i -g clackr
+```
+
+Create a room
+
+```bash
+  clackr-host
+```
+Share the room url with others.
+To join a room run
+
+```bash
+clackr-join
+```
+and enter your name and url.
+## Features
+
+- Infinite Rooms can be created
+- No limit on number of users
+- Collects zero data
+- Extremely secure
+- Chats are never stored, and are deleted once room is destroyed.
+
+
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  npm i clackr
+  npm run host
+  npm run join
+```
+
+
+## Authors
+
+- [@aarush-paul](https://www.github.com/aarush-paul)
+
+
+## License
+
+[Creative COmmons Zero v1.0 Universal](https://choosealicense.com/licenses/mit/)
+
---
+++ server.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+/*
+Clakr | Made by Aarush Paul
+Github: https://github.com/aarush-paul
+*/
+console.clear();
+const CLI = require('clui');
+const Spinner = CLI.Spinner;
+var countdown = new Spinner('Importing modules  ', ['◜','◠','◝','◞','◡','◟']);
+var countdown2 = new Spinner('Starting Up  ', ['◜','◠','◝','◞','◡','◟']);
+var countdown3 = new Spinner('', [' ',' ',' ',' ',' ',' ']);
+countdown.start();
+const io = require("socket.io")();
+const chalk = require("chalk");
+const prompt = require("prompt-sync")({ sigint: true });
+const localtunnel = require('localtunnel');
+console.log(chalk.bold.blue("         ,,                                    "));
+console.log(chalk.bold.blue("       `7MM                 `7MM               "));
+console.log(chalk.bold.blue("         MM                   MM               "));
+console.log(chalk.bold.blue(` ,p6"bo  MM   ,6"Yb.  ,p6"bo  MM  ,MP' 7Mb,od8 `));
+console.log(chalk.bold.blue(`6M'  OO  MM  8)   MM 6M'  OO  MM ;Y     MM' "' `));
+console.log(chalk.bold.blue("8M       MM   ,pm9MM 8M       MM;Mm     MM     "));
+console.log(chalk.bold.blue("YM.    , MM  8M   MM YM.    , MM `Mb.   MM     "));
+console.log(chalk.bold.blue(" YMbmd'.JMML.`Moo9^Yo.YMbmd'.JMML. YA..JMML.   "));
+console.log(chalk.bold.blue(" ██████╗███████╗██████╗ ██╗   ██╗███████╗██████╗   "));
+console.log(chalk.bold.blue("██╔════╝██╔════╝██╔══██╗██║░░░██║██╔════╝██╔══██╗  "));
+console.log(chalk.bold.blue("╚█████╗░█████╗░░██████╔╝╚██╗░██╔╝█████╗░░██████╔╝  "));
+console.log(chalk.bold.blue("░╚═══██╗██╔══╝░░██╔══██╗░╚████╔╝░██╔══╝░░██╔══██╗  "));
+console.log(chalk.bold.blue("██████╔╝███████╗██║░░██║░░╚██╔╝░░███████╗██║░░██║  "));
+console.log(chalk.bold.blue("╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  "));
+console.log(" ");
+console.log(chalk.bold.blue("Made By Aarush Paul"));
+console.log(" ");
+console.log(chalk.yellow("https://github.com/aarush-paul/cli-chat"));
+console.log(" ");
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+sleep(5000)
+  .then(() => countdown.stop())
+  .then(() => countdown2.start())
+  .then(() => sleep(2000))
+  .then(() => countdown2.stop())
+
+sleep(8000).then(() => {
+const port = prompt("Enter the port you want to use (must be between 1025 and 65535): ");
+if (port < 1025 || port > 65535) {
+    console.log(chalk.bold.red("[!] Invalid Port!"));
+    process.exit();
+}
+const users = {};                                                                                                                  
+io.listen(port);
+
+(async () => {
+  const tunnel = await localtunnel({ port: port});
+  console.log(chalk.bold.green("[i] Your chat url is:  " + chalk.bold.red(tunnel.url) + ". Run `clacker join` on another terminal window and enter this url to join. Send this url to your friends and ask them to paste this link in the console after running `clackr join`. "));
+})();
+
+console.log(" ");
+console.log(chalk.bold.bgGreen("=====CHATROOM LOGS(Press CTRL+Q to destroy room)====="));
+
+    io.on("connection", (socket) => {
+	    console.log(chalk.bold.cyan("[!] New Connection: ") + chalk.dim.underline(socket.id));
+        socket.on('new user', (name) => {
+            users[socket.id] = chalk.bold.yellow(name);
+            socket.broadcast.emit("message", ` `);
+            socket.broadcast.emit("message", chalk.bgYellow(`${name} joined the chat.`));
+            socket.broadcast.emit("message", ` `);
+        });
+
+        socket.on('message', (text) => {
+            socket.broadcast.emit("message", `${users[socket.id]} > ${text}`);
+        });
+    });
+    var stdin = process.stdin;
+    stdin.setRawMode( true );
+    stdin.resume();
+    stdin.setEncoding( 'utf8' );
+    stdin.on( 'data', function( key ){
+      // ctrl-c ( end of text )
+      if ( key === '\u0011' ) {
+        (async function() {await ngrok.disconnect(url);}());
+        process.exit();
+      }
+      process.stdout.write( key );
+    });
+  });
